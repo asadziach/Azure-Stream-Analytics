37f1f1f1db9f99e2d966edd603e3916c8b1b1fb5
Asad Zia <asad_zia@mentor.com>
2016-03-19 19:52:42 +0500

    Added sketch.

---
+++ sketch/MKR1000_Azure_Heater/MKR1000_Azure_Heater.ino
@@ -0,0 +1,45 @@
+// Copyright (c) Arduino. All rights reserved.
+// Copyright (c) Asad Zia
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <WiFi101.h>
+
+#include "azure_http.h"
+
+// This file is not added to github because every user has different wifi and cloud credentials.
+#include "network_credentials.h" 
+
+char ssid[] = WIFI_SSID;        //  Create network_credentials.secret and define there.
+char pass[] = WIFI_PASSWORD;    //  Create network_credentials.secret and define there.
+
+int status = WL_IDLE_STATUS;
+
+void setup() {
+  Serial.begin(9600);
+
+  // check for the presence of the shield :
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    if (status != WL_CONNECTED) {
+      // wait 10 seconds for connection:
+      delay(10000);
+    }
+  }
+  Serial.println("Connected to wifi");
+}
+
+void loop() {
+  azure_http_run();
+}
+
---
+++ sketch/MKR1000_Azure_Heater/azure_http.h
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Asad Zia
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef SIMPLESAMPLEHTTP_H
+#define SIMPLESAMPLEHTTP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void azure_http_run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SIMPLESAMPLEHTTP_H */
